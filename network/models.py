1ba54ab9952711683144175cb40cd7494da5e40c
mamedxanli@gmail.com <mamedxanli@gmail.com>
2021-09-13 09:00:02 +0000

    regular db backup

--- network/models.py
+++ network/models.py
@@ -16,18 +16,18 @@ class NetworkHardware(models.Model):
     created_by = models.CharField(max_length=100, default=None)
 
     # Model
-    code = models.IntegerField("Kod", primary_key=True)
-    brand = models.CharField("Brend", max_length=20, default=None)
+    code = models.IntegerField("Code", primary_key=True)
+    brand = models.CharField("Brand", max_length=20, default=None)
     model = models.CharField("Model", max_length=50)
-    serial_number = models.CharField("Seriya Nömrəsi", max_length=50, default=0)
-    category = models.CharField("Kateqoriya", max_length=30, default=None)
-    description = models.TextField("Təsvir", max_length=500, default=None)
-    purchase_price = models.DecimalField("Alış qiməti", decimal_places=2, max_digits=10)
-    selling_price = models.DecimalField("Satış qiyməti", decimal_places=2, max_digits=10)
-    quantity = models.IntegerField("Ədəd", default=1)
-    image = models.FileField("Şəkil",blank=True, default=None, upload_to=save_directory_path)
-    sold = models.BooleanField("Satılıb", default=False)
-    inventory = models.BooleanField("Inventar", default=False)
+    serial_number = models.CharField("Serial Number", max_length=50, default=0)
+    category = models.CharField("Category", max_length=30, default=None)
+    description = models.TextField("Description", max_length=500, default=None)
+    purchase_price = models.DecimalField("Purchase price", decimal_places=2, max_digits=10)
+    selling_price = models.DecimalField("Selling pricei", decimal_places=2, max_digits=10)
+    quantity = models.IntegerField("Quantity", default=1)
+    image = models.FileField("Image",blank=True, default=None, upload_to=save_directory_path)
+    sold = models.BooleanField("Sold", default=False)
+    inventory = models.BooleanField("Inventory", default=False)
     
     def __str__(self):
         return " {} ".format(self.code)
@@ -54,7 +54,7 @@ class NetworkHardware(models.Model):
         """
         Handy way of getting the class of the object for the html templates
         """
-        class_name = "Şəbəkə avadanlığı"
+        class_name = "Class name"
         return class_name
     
     def get_amount_in_stock(self):
@@ -72,4 +72,4 @@ class NetworkHardware(models.Model):
         return number
 
     class Meta:
-        verbose_name_plural = "Şəbəkə avadanlıqları"
+        verbose_name_plural = "Verbose name"
